6801d938c7240f9141defc0bd95a6f4fc8648c55
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2023-12-07 02:03:02 +0000

    looking good

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import TodoList from "./ToDoList";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">My ToDo List</h1>
+			<TodoList />
 
 		</div>
 	);
